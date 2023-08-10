fd858dd753b4bbac0fee30ea57a4bc2bc7df75c3
Lolillya <SomeonesUsernam@gmail.com>
2023-08-10 19:37:49 +0800

    final output

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿namespace exp1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            int pinNumber = 123;
+            Boolean isMatch = false;
+
+            for (int i = 0; i < 3; i++) {
+                Console.Write("Please enter the PIN: ");
+                int pinAttemp = Convert.ToInt32(Console.ReadLine());
+
+                if (pinAttemp == pinNumber) {
+                    isMatch = true;
+                    break;
+                    }
+                Console.WriteLine("Incorrect PIN");
+            }
+
+            if (isMatch)
+                Console.WriteLine("Access Granted");
+            else
+                Console.WriteLine("Sorry your account will be temporarily suspended. Visit your nearest branch for reactivation");
+            
+        }
+    }
+}
